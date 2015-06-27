8d07ecee48444615776b70b9b5e88a3dbb9f7357
wdl <weidoalong@outlook.com>
2015-06-27 23:47:33 +0800

    tt

--- src/main/resources/sql/mysql/schema.sql
+++ src/main/resources/sql/mysql/schema.sql
@@ -2,7 +2,7 @@ drop table if exists ss_team;
 
 create table ss_team (
   	id bigint auto_increment,
-	name varchar(255) not null unique,
+	name varchar(255) not null,
 	master_id bigint,
     primary key (id)
 ) engine=InnoDB;
--- src/test/java/com/wdl/foo/repository/mybatis/TeamDaoTest.java
+++ src/test/java/com/wdl/foo/repository/mybatis/TeamDaoTest.java
@@ -3,7 +3,7 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  *******************************************************************************/
-package org.springside.examples.showcase.repository.mybatis;
+package com.wdl.foo.repository.mybatis;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,21 +11,30 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+import org.springframework.test.context.transaction.TransactionConfiguration;
 
-import com.wdl.foo.entity.Team;
-import com.wdl.foo.repository.mybatis.TeamDao;
+import com.wdl.foo.service.AccountEffectiveService;
+
+
+/**
+ * @TransactionConfiguration(transactionManager="transactionManager",defaultRollback=false)
+ * 设置事务默认提交
+ * @author wdl
+ *
+ */
+
+
 
 @DirtiesContext
-@ActiveProfiles("test")
+@ActiveProfiles("development")
 @ContextConfiguration(locations = { "/applicationContext.xml" })
+@TransactionConfiguration(transactionManager="transactionManager",defaultRollback=false)
 public class TeamDaoTest extends AbstractTransactionalJUnit4SpringContextTests {
 
 	@Autowired
-	private TeamDao teamDao;
+	private AccountEffectiveService teamDao;
 
 	@Test
-	public void getTeamWithDetail() throws Exception {
-		Team team = teamDao.getWithDetail(1L);
-		System.out.println(team.getName());
-	}
+	public void getTeamWithDetail() throws Exception {}
+	
 }
