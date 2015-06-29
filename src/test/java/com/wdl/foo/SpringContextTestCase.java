5d3a1810708978022b82e1b1da50c72754b4ade3
wdl <weidoalong@outlook.com>
2015-06-29 22:54:43 +0800

    test

--- src/test/java/com/wdl/foo/SpringContextTestCase.java
+++ src/test/java/com/wdl/foo/SpringContextTestCase.java
@@ -2,8 +2,8 @@ package com.wdl.foo;
 
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
-
-@ActiveProfiles("development")
+//development
+@ActiveProfiles("test")
 public class SpringContextTestCase extends AbstractJUnit4SpringContextTests {
 
 }
